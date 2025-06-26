ce6719e764bebf8ca24b72904be3fc4ed5ee5bf4
Tristan Clark <tcjc107@gmail.com>
2025-06-26 13:07:47 -0700

    made updates to board

--- SudokuSolver/SudokuSolver.cpp
+++ SudokuSolver/SudokuSolver.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 #include <vector>
+#include <random>
+#include <unordered_map>
 
 int rows = 9;
 int columns = 9;
 std::vector<std::vector<int>> board(rows, std::vector<int>(columns,0));
+std::vector<std::vector<int>> answerBoard(rows, std::vector<int>(columns,0));
+
+using OptionsMap = std::unordered_map<int, std::unordered_map<int, std::vector<int>>>;
+
+void initializeOptions(OptionsMap& cellOptions)
+{
+    for(int row = 0; row < 9; row++)
+    {
+        for(int col = 0; col < 9; col++)
+        {
+            cellOptions[row][col] = {1,2,3,4,5,6,7,8,9};
+        }
+    }
+}
 
 /*
 std::vector<std::vector<int>> populateBoard()
@@ -47,7 +63,28 @@ void populateBoard()
     }
 }
 
-void printBoard()
+void prepareBoard()
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dist(0.0,1.0);
+
+    double r = 0.0;
+
+    for(int i = 0; i < columns; i++)
+    {
+        for(int j = 0; j < rows; j++)
+        {
+            r = dist(gen);
+            if(r < 0.85)
+            {
+                board[i][j] = 0;
+            }
+        }
+    }
+}
+
+void printBoard(std::vector<std::vector<int>> board)
 {
     for(int i = 0; i < columns; i++)
     {
@@ -57,11 +94,62 @@ void printBoard()
         }
         std::cout << std::endl;
     }
+    std::cout << std::endl;
+}
+
+void solveSudoku(std::vector<std::vector<int>> board, OptionsMap& cellOptions)
+{
+    /*
+     * for i in columns
+     *  for j in rows
+     *      if(board[i][j] is 0
+     *          then we need to find it
+     *          check the rest of row at column i
+     *          check row and column for 
+     *          what numbers can it be
+     *          what if we had a map with board[i][j] as the key
+     *          at that map we had list of numbers it can be.
+     *          it starts our full and then we start popping numbers off
+     *          as we check the row, column and square that it is in.
+     *          I know how to check the row, and column but don't know how to do square rn
+     *
+     */
+    for(int i = 0; i < columns; i++)
+    {
+        for(int j = 0; j < rows; j++)
+        {
+            if(board[i][j] == 0)
+            {
+                for(int k = 0; k < rows; k++)
+                {
+                    if(board[k][j] != 0)
+                    {
+                        auto& options cellOptions[i][j];
+                        options.erase(std::remove(options.begin(), options.end(), board[k][j]), options.end()):
+                    }
+                }
+            }
+            else
+            {
+                cellOptions[i][j] = {board[i][j]};
+            }
+        }
+    }
 }
 
 int main()
 {
+    OptionsMap cellOptions;
+
+    initializeOptions(cellOptions);
     populateBoard();
-    printBoard();
+    answerBoard = board;
+    printBoard(board);
+    printBoard(answerBoard);
+
+    prepareBoard();
+    printBoard(board);
+    printBoard(answerBoard);
+
     return 0;
 }
